1230b94f93b91c8f88a5bfe4cc78029261b8223f
kundu <kundu.sourodip@outlook.com>
2024-06-24 01:48:34 +0530

    added commit for data table filters

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './components/Pages/WelcomePage';
 import RegisterPage from './components/Pages/RegisterPage';
 import MultiStepFormPage from './components/Pages/MultiStepFormPage';
-import DataTableComponent from './components/Pages/DataModel';
+import DataTables from './components/DataTable/DataTable';
 
 const App: React.FC = () => {
   return (
@@ -12,7 +12,7 @@ const App: React.FC = () => {
         <Route path='/' Component={WelcomePage} />
         <Route path='/register' Component={RegisterPage} />
         <Route path='/multi-step' Component={MultiStepFormPage} />
-        <Route path='/data-table' Component={DataTableComponent} />
+        <Route path='/data-table' Component={DataTables} />
       </Routes>
     </Router>
   );
